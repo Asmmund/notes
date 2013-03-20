aefe95721171fa5045778ad51fe2f234d8b5691b
Asmmund <antony.ermolenko@gmail.com>
2013-03-20 22:07:09 +0200

    [+]Added simpleconv gem

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,13 +4,15 @@ describe User do
   it { should have_many(:articles) }
   it { should validate_presence_of(:name)}
   it 'should validate uniqueness of name' do
-    User.new(password:'whatever', name: 'sdf',email:'something@s.com').save!(:validate => false)
-
+    user_invalid
     should validate_uniqueness_of(:name)
   end
   it { should validate_presence_of(:email)}
   it 'Should validate uniqueness of email' do
-    User.new(password:'whatever', name: 'sdf',email:'something@s.com').save!(:validate => false)
+   user_invalid
    should validate_uniqueness_of(:email)
+  end
+  def user_invalid
+      User.new(password:'whatever', name: 'sdf',email:'something@s.com').save!(:validate => false)
   end
 end
